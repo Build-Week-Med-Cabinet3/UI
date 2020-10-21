eb983502bdb77b4ea7f2023d5e6f87122c12a973
Jesse Medrano <Jtamedrano@gmail.com>
2020-10-20 20:13:09 -0700

    Style ammended and links to Front End Deployment

--- med_cabinet_ui_marketing_page/src/Components/UI_DISP_JOIN.js
+++ med_cabinet_ui_marketing_page/src/Components/UI_DISP_JOIN.js
@@ -14,16 +14,19 @@ export default function UI_DISP_JOIN({ colorPrimary, colorSecondary }) {
           'dd 100%),url("https://assets.newatlas.com/dims4/default/b132701/2147483647/strip/true/crop/1992x1328+4+0/resize/1200x800!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2F1c%2F0c%2F920c17e34fff91c2c8b0ada1c1bb%2Fdepositphotos-66142845-l-2015.jpg")',
       }}
     >
-      <Button
+      <a
         style={{
           backgroundColor: colorSecondary,
           border: "1px solid " + colorSecondary,
           fontSize: "2.5rem",
           boxShadow: "0 1px 8px rgb(25,25,25)",
+          padding: ".375rem .75rem",
+          color: "white",
         }}
+        href="https://med-cabinet-dusky.vercel.app/register"
       >
         Join Today
-      </Button>
+      </a>
     </section>
   );
 }
--- med_cabinet_ui_marketing_page/src/Components/UI_NAV.js
+++ med_cabinet_ui_marketing_page/src/Components/UI_NAV.js
@@ -9,24 +9,29 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import "../style.css";
+import logo from "../res/img/mcLogo.png";
 
 const UI_NAV = ({ primary }) => {
   return (
     <Navbar style={{ backgroundColor: primary }} dark className="UI_NAV">
       <Link to="/">
-        <NavbarBrand>Med Cabinet Logo</NavbarBrand>
+        <NavbarBrand>
+          <img src={logo} alt="Med Cabinet Logo" style={{ height: "2em" }} />
+        </NavbarBrand>
       </Link>
       <div className="UI_NAV_LINKS">
         <Collapse isOpen={true} navbar>
           <Nav navbar>
             <div className="UI_NAV_ITEMS">
               <NavItem>
-                <Link to="/">
-                  <NavLink href="#">Join Now</NavLink>
-                </Link>
+                <NavLink href="https://med-cabinet-dusky.vercel.app/register">
+                  Join Now
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="/">Log In</NavLink>
+                <NavLink href="https://med-cabinet-dusky.vercel.app/">
+                  Log In
+                </NavLink>
               </NavItem>
             </div>
           </Nav>
